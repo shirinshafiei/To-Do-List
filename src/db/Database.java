00f0b65da2553f85bf93fad9520f38fb3512dc7b
Shirin Shafiei <shirin84shafiei@gmail.com>
2025-03-27 19:03:39 +0330

    Initial database design setup

---
+++ src/db/Database.java
@@ -0,0 +1,45 @@
+package db;
+
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+public class Database {
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    public static int ID = 1;
+
+    public static void add(Entity e) {
+        if (e == null) {
+            throw new IllegalArgumentException("Entity cannot be null");
+        }
+        e.id = ID;
+        ID++;
+        entities.add(e);
+    }
+
+    public static Entity get(int id) {
+        for (Entity e : entities) {
+            if (e.id == id) {
+                return e;
+            }
+        }
+        throw new EntityNotFoundException("Entity not found with id: " + id);
+    }
+
+    public static void delete(int id) {
+        Entity entity = get(id);
+        entities.remove(entity);
+    }
+
+    public static synchronized void update(Entity e) {
+        if (e == null) {
+            throw new IllegalArgumentException("Entity cannot be null");
+        }
+
+        Entity existingEntity = get(e.id);
+
+        int index = entities.indexOf(existingEntity);
+        entities.set(index, e);
+    }
+
+}
---
+++ src/db/exception/EntityNotFoundException.java
@@ -0,0 +1,18 @@
+package db.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+
+    public EntityNotFoundException() {
+        super("Cannot find entity");
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(int id) {
+        super(String.format("Cannot find entity with id=%d", id));
+    }
+}
+
